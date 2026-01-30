b563c864c61155ff9be42c7a471b4d429a29cba9
Nabin Dhakal <nbn@Nabins-MacBook-Pro.local>
2026-01-30 19:17:31 +1100

    version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 allprojects {
     // Maven Central coordinates (namespace verified)
     group = "com.flutterjunction.formflow"
-    version = "0.1.4-alpha"
+    version = "0.1.3-alpha"
 }
 
 subprojects {
